a1d7fef28b3c2bc61bab5c10d5402bd4c4a5d371
kuntaro0524 <37730000+kuntaro0524@users.noreply.github.com>
2021-10-07 10:27:36 +0900

    Omitを利用して props の一部を無視する指定方法 共通定義した型指定オブジェクトなどの中で、一部、共通で利用しないということを指定する必要がComponent側ではよくあるはず（不要なものを指定しない） このときに PickとOmitというものを利用して、型の一部を抽出したり、無視したりする。

--- src/Todo.tsx
+++ src/Todo.tsx
@@ -3,7 +3,10 @@ import { TodoType } from "./types/todo";
 // 関数の型指定には ":"　で区切って()内に入れる
 export const Todo = (
   // TodoTypeの中身の変数のうち、一部だけを利用したい場合にはこのように設定が可能
-  props: Pick<TodoType, "userId" | "title" | "completed">
+  // Pick or Omit
+  // 無視するのが多いか少ないかで決めていけば良い気がする
+  // id だけ（一つだけ）を省略したい場合には omit の方が自然だよねってお話
+  props: Omit<TodoType, "id">
 ) => {
   const { title, userId, completed } = props;
   const tmpText = completed ? "完了" : "未完";
