770c1462bc9b35aae946ce32f544dc196c3c1cfc
kuntaro0524 <37730000+kuntaro0524@users.noreply.github.com>
2021-10-07 10:44:17 +0900

    debug of the previous commit

--- src/Todo.tsx
+++ src/Todo.tsx
@@ -1,12 +1,12 @@
 import { TodoType } from "./types/todo";
 
 // 関数の型指定には ":"　で区切って()内に入れる
-export const Todo = (
+export const Todo: VFC<Omit<TodoType, "id">> = (
   // TodoTypeの中身の変数のうち、一部だけを利用したい場合にはこのように設定が可能
   // Pick or Omit
   // 無視するのが多いか少ないかで決めていけば良い気がする
   // id だけ（一つだけ）を省略したい場合には omit の方が自然だよねってお話
-  props: Omit<TodoType, "id">
+  props
 ) => {
   const { title, userId, completed } = props;
   const tmpText = completed ? "完了" : "未完";
