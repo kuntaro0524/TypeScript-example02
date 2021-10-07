47bfb138dfe2579f8ef29243293286552317166b
kuntaro0524 <37730000+kuntaro0524@users.noreply.github.com>
2021-10-07 09:55:43 +0900

    Propsに対して型指定をして挙動を確かめた props に型指定すると、渡す側、受ける側でバグを探しやすくなるので指定することは必須

--- src/Todo.tsx
+++ src/Todo.tsx
@@ -1,4 +1,13 @@
-export const Todo = (props) => {
-  const { title, userid } = props;
-  return <p> {`${title}(ユーザ：${userid})`} </p>;
+// ?を追加するとそれは必須ではないということを示す
+type TodoType = {
+  userId: number;
+  title: string;
+  completed?: boolean;
+};
+
+// 関数の型指定には ":"　で区切って()内に入れる
+export const Todo = (props: TodoType) => {
+  const { title, userId, completed } = props;
+  const tmpText = completed ? "完了" : "未完";
+  return <p> {`${tmpText} ${title}(ユーザ：${userId})`} </p>;
 };
