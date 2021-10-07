b96e8a558849275b72e9c3171f5ded25838e6b3a
kuntaro0524 <37730000+kuntaro0524@users.noreply.github.com>
2021-10-07 09:31:18 +0900

    useStateで管理する変数を型指定する方法 JSONのリストをとってきて、それぞれの内包変数に型を指定する

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,24 @@ import { useState } from "react";
 import "./styles.css";
 import { Todo } from "./Todo";
 
+type TodoType = {
+  userId: number;
+  id: number;
+  title: string;
+  completed: boolean;
+};
+
 export default function App() {
   // 共通変数とセット関数を定義する
   // これにToDoリストを保持して使っていく
-  const [todos, setTodos] = useState<any>([]);
+  const [todos, setTodos] = useState<Array<TodoType>>([]);
 
   const onClickFetchData = () => {
-    axios.get("https://jsonplaceholder.typicode.com/todos").then((res) => {
-      setTodos(res.data);
-    });
+    axios
+      .get<Array<TodoType>>("https://jsonplaceholder.typicode.com/todos")
+      .then((res) => {
+        setTodos(res.data);
+      });
   };
   return (
     <div className="App">
